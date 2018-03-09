d8391df4ad3cebfb4cbb772bcac536c11b57d128
TECH\zhaogc <13001253655@163.com>
2018-03-09 18:23:48 +0800

    快速排序

---
+++ Sort.java
@@ -0,0 +1,45 @@
+
+    public class Sort {
+        public static void main(String [] args){
+            int [] a = {1,3,0,3,6,5,8,2};
+
+            Sort(a, 0, a.length -1);
+            for (int i  =0; i<8;i++) {
+                System.out.print(a[i]+"，");
+            }
+
+
+            System.out.print("\n");
+        }
+
+        public static void Sort(int [] res, int start , int end){
+            if(start >= end){
+                return;
+            }
+
+            int mid = quickSort(res, start, end);
+
+            Sort(res, start, mid-1);
+
+
+            Sort(res, mid +1,end);
+
+        }
+        public static int quickSort(int [] res , int lift, int right){
+            int tmp = res[lift];
+            while(lift<right){
+                while(lift<right&&res[right]>=tmp){
+                    --right;
+                }
+                res[lift] = res[right];
+                while(lift<right&&res[lift]<=tmp){
+                    ++lift;
+                }
+                res[right] = res[lift];
+            }
+            res[lift] = tmp;
+            return lift;
+        }
+    }
+
+
